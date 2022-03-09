3159f002b41c27911c279db2babf58c403c0eb7b
Flavia Kosti <flaviakosti@gmail.com>
2022-03-09 21:10:42 +0100

    basic layout/structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevicesComponent } from './base/devices/devices.component';
+import { DeviceDetailsComponent } from './base/device-details/device-details.component';
+import { BaseComponent } from './base/base.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DevicesComponent,
+    DeviceDetailsComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    FormsModule,
+    MatIconModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
